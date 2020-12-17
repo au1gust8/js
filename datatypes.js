ed992d764c574563866f36adfd0abffc4ca1d0af
helle99 <au1gust8@gmail.com>
2020-12-18 00:07:52 +0900

    second commit

---
+++ datatypes.js
@@ -0,0 +1,64 @@
+let whatever = 'Mark'; //문자열타입가진 변수
+
+whatever = 37; //whatever는 숫자형 타입이 됨
+
+whatever = true; //불리언 타입이 됨
+
+//값에 따라 타입이 달라짐 이것을 동적타입이라고함 danamic 변수가 갖는 고정 타입이 없다
+
+//primitivce value 기본타입 (boolean null undefined number string symbol)
+
+//boolean
+const isTrue = true;
+const isFalse = false;
+
+console.log(isTrue,typeof isTrue);
+console.log(isFalse,typeof isFalse);
+
+new a = Boolean(false);
+
+//Null
+const a = null;
+
+console.log(a, typeof a);
+
+//Undefined 
+let b;
+
+console.log;ongotpointercapture(b, typeof b);
+
+b = undefined;
+
+console.log(b, typeof b);
+
+if (a == b){
+    console.log(a==b);
+}
+
+if (a === b){
+    console.log(a === b);
+}
+
+//Number
+const a = 37;
+
+console.log(a, typeof a)length
+
+const b = 96.7;
+
+console.log(b, typeof b);
+
+const c = NAN;
+console(c, typeof c);
+const d = Number('Mark';
+console.log (d, typeof d);
+
+const e = Number('37');
+console.log(e, typeof e);
+
+//String
+const a = "Mark"
+
+console.log(a, typeof a);
+
+const b = 'Mark' + 'Son';
